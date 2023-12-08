2b2101539d24f1c9cbebcdf1e52d2c230b203759
kieranwatt <kieranmwatt@gmail.com>
2023-12-08 08:55:45 -0500

    bruh

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,2 +1,15 @@
-import '../styles/style.css'
-const URL = https://hacker-news.firebaseio.com/v0/
+import '../styles/style.css';
+const URL = "https//hacker-news.firebaseio.com/v0/"
+
+async function getData(URL) {
+    try{
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data.content);
+    } catch (error) {
+        console.log(error);
+    }
+
+    
+}
+getData(URL);
