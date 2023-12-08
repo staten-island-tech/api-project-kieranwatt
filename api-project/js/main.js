359c2b702a1dba5759154b83ab34360d1cb60f80
kieranwatt <kieranmwatt@gmail.com>
2023-12-08 09:24:52 -0500

    logged bus lane data

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,15 +1,9 @@
 import '../styles/style.css';
-const URL = "https//hacker-news.firebaseio.com/v0/"
+const URL = "https://data.cityofnewyork.us/resource/ycrg-ses3.json"
 
 async function getData(URL) {
-    try{
     const response = await fetch(URL);
     const data = await response.json();
-    console.log(data.content);
-    } catch (error) {
-        console.log(error);
+    console.log(data);
     }
-
-    
-}
 getData(URL);
