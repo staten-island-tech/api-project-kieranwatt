68a80c0572e1bb627d83e73dab5ca3fa10b8f4bf
kieranwatt <kieranmwatt@gmail.com>
2023-12-13 09:26:16 -0500

    12/13/23

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -5,8 +5,15 @@ async function getData(URL) {
     const response = await fetch(URL);
     const data = await response.json();
     console.log(data);
-    }
+    const boro = data.filter(boro => boro.bor_ === "MAN");
+    console.log(boro); 
+    const bus = data.filter(bus => bus.sbs_route2 === "M14 D");
+    console.log(bus);
+    const lanebusway = data.filter(lane => lane.lane_type === "Busway");
+    console.log(lanebusway);
+    const lanesingle = data.filter(lane => lane.lane_type === "Single");
+    console.log(lanesingle);
+    const lanedual = data.filter(lane => lane.lane_type === "Dual");
+    console.log(lanedual);
+    };
 getData(URL);
-
-/* const boro = data.filter(data => data.bor_ === "QNS");
-console.log(boro); */
