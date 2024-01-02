9ce5b93049439da242aa526bc4d68f5c55713ee1
kieranwatt <kieranmwatt@gmail.com>
2024-01-01 23:02:49 -0500

    ill figure everything out tomorrow stuck on how too do js

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -6,8 +6,8 @@ async function getData(URL){
     try {
         const response = await fetch(URL);
         const data = await response.json();
-        // console.log(data);   
-        document.getElementById("api-response").textContent = data.content;
+        console.log(data);   
+        // document.getElementById("api-response").textContent = data.content;
     } catch (error) {
         console.log(error);
     }
@@ -16,3 +16,8 @@ async function getData(URL){
 getData(URL);
 
 // Idea of the project -allows you too filter by boro and lane type too see what sbs busses operate on it
+
+//use buttons to make 1 card pop up showing the m15 image as the header and a list of sbs routes for that filter (ie. "Staten Island" would show "S 79" and "Busway" would show "M 14A, M 14D, etc")
+//remove button too remove the card too allow you too use other filter buttons
+
+//if there is time make website look much better
