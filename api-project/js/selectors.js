5029a5076785f78a85d47c169d43d584bfec0384
kieranwatt <kieranmwatt@gmail.com>
2024-01-01 22:33:23 -0500

    little css edit and dom selector work (alledgedly)

---
+++ api-project/js/selectors.js
@@ -0,0 +1,13 @@
+const domselectors = {
+    cards: document.getElementById("cards"),
+    looks: document.getElementById("looks"),
+    brooklyn: document.getElementById("Brooklynlanes"),  
+    statenisland: document.getElementById("StatenIslandlanes"),  
+    manhattan: document.getElementById("Manhattanlanes"),
+    queens: document.getElementById("Queenslanes"),
+    thebronx: document.getElementById("Bronxlanes"),
+    busway: document.getElementById("busway"),
+    single: document.getElementById("single"),
+    dual: document.getElementById("dual"),
+};
+export { domselectors };
