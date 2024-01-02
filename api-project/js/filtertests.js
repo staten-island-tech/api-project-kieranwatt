63912987005059c3b0bc255395df0796eb69f8ef
kieranwatt <kieranmwatt@gmail.com>
2024-01-01 21:32:53 -0500

    BRO I FORGOT ABT THIS IM ABT TOO CLUTCH UP

---
+++ api-project/js/filtertests.js
@@ -0,0 +1,25 @@
+import '../styles/style.css';
+const URL = "https://data.cityofnewyork.us/resource/ycrg-ses3.json"
+
+async function getData(URL) {
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data);
+    //search bar 
+    const boro = data.filter(boro => boro.bor_ === "MAN");
+    console.log(boro); 
+    //make search bar
+    const bus = data.filter(bus => bus.sbs_route2 === "M14 D");
+    console.log(bus);
+    //make the next buttons
+    const lanebusway = data.filter(lane => lane.lane_type === "Busway");
+    console.log(lanebusway);
+    const lanesingle = data.filter(lane => lane.lane_type === "Single");
+    console.log(lanesingle);
+    const lanedual = data.filter(lane => lane.lane_type === "Dual");
+    console.log(lanedual);
+    //make red lane a button
+    const color = data.filter(color=> color.prim_color === "Red");
+    console.log(color);
+    };
+getData(URL);
--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,25 +1,17 @@
 import '../styles/style.css';
+// import { domselectors } from "./selectors";
 const URL = "https://data.cityofnewyork.us/resource/ycrg-ses3.json"
 
-async function getData(URL) {
-    const response = await fetch(URL);
-    const data = await response.json();
-    console.log(data);
-    //search bar 
-    const boro = data.filter(boro => boro.bor_ === "MAN");
-    console.log(boro); 
-    //make search bar
-    const bus = data.filter(bus => bus.sbs_route2 === "M14 D");
-    console.log(bus);
-    //make the next buttons
-    const lanebusway = data.filter(lane => lane.lane_type === "Busway");
-    console.log(lanebusway);
-    const lanesingle = data.filter(lane => lane.lane_type === "Single");
-    console.log(lanesingle);
-    const lanedual = data.filter(lane => lane.lane_type === "Dual");
-    console.log(lanedual);
-    //make red lane a button
-    const color = data.filter(color=> color.prim_color === "Red");
-    console.log(color);
-    };
+async function getData(URL){
+    try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        // console.log(data);   
+        document.getElementById("api-response").textContent = data.content;
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
 getData(URL);
+
