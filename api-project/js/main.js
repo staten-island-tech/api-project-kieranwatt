90e2ae3ed77dee25551c35c92df83fe62c526993
kieranwatt <kieranmwatt@gmail.com>
2024-01-07 19:03:58 -0500

    edgemoggers roblox skibidi toilet (lot of work)

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -17,56 +17,82 @@ async function fetchData(url) {
 
 async function processRoutes(url) {
     try {
-      
+
         const data = await fetchData(url);
 
         const uniquesbsroute1 = new Set();
-       // const uniquesbsroute2 = new Set();
-        //const test = new Set()
         data.forEach(entry => {
             uniquesbsroute1.add(entry.sbs_route1);
             uniquesbsroute1.add(entry.sbs_route2);
-
-           // uniquesbsroute2.add(entry.sbs_route2);
         });
         console.log(uniquesbsroute1)
-        //insert card code here
-        const store = function(bladeebus) {
-            domselectors.cards.insertAdjacentHTML("beforeend",
-            ` <div class="card" id="allcards">
-              <img src="m15sbs.jpg" alt="img" class="card-img">
-              <h4 class="card-type">${bladeebus.sbs_route1}</h4>
-              <h4 class="card-desc">${bladeebus.sbs_route2}</h5>
-              </div>
-              `)
-        }; //for this code figure out how it pulls data from the code below
-        uniquesbsroute1.forEach(value1 => {
-            if(value1){
-                console.log(value1);
-                //insert data into card instead of logging data
+        function cardinsert(bladeebus) {
+            if (bladeebus.sbs_route2) {
+                domselectors.cards.insertAdjacentHTML("beforeend",
+                    `<div class="card hidden" id="allcards">
+                    <img src="m15sbs.jpg" alt="img" class="card-img">
+                    <h4 class="card-route1">${bladeebus.sbs_route1}</h4>
+                    <h4 class="card-route2">${bladeebus.sbs_route2}</h5>
+                </div>`
+                );
+            } else {
+                domselectors.cards.insertAdjacentHTML("beforeend",
+                    `<div class="card hidden" id="allcards">
+                    <img src="m15sbs.jpg" alt="img" class="card-img">
+                    <h4 class="card-route1">${bladeebus.sbs_route1}</h4>
+                </div>`
+                );
             }
-            
-        }); 
+        }
+        uniquesbsroute1.forEach(value1 => {
+            if (value1) {
+                const filteredData = data.filter(entry => entry.sbs_route1 === value1 || entry.sbs_route2 === value1);
+                cardinsert(filteredData[1])
+            }
+        });
     } catch (error) {
         console.error(error.message);
     }
+
 }
 
-const boros = ["MAN", "BK", "QNS", "SI", "BX"];
-const laneTypes = ["Busway", "Single"];
-
-boros.forEach(boro => {
-    const url = `https://data.cityofnewyork.us/resource/ycrg-ses3.json?bor_=${boro}`;
-    processRoutes(url);
+const filterButtons = document.querySelectorAll('.filterbtns');
+filterButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        document.querySelectorAll("#allcards").forEach((card) => card.remove());
+        if (button.textContent === "Brooklyn") {
+            const url = `https://data.cityofnewyork.us/resource/ycrg-ses3.json?bor_=BK`
+            processRoutes(url)
+        }
+        else if (button.textContent === "Staten Island") {
+            const url = `https://data.cityofnewyork.us/resource/ycrg-ses3.json?bor_=SI`
+            processRoutes(url)
+        }
+        else if (button.textContent === "Manhattan") {
+            const url = `https://data.cityofnewyork.us/resource/ycrg-ses3.json?bor_=MAN`
+            processRoutes(url)
+        }
+        else if (button.textContent === "Queens") {
+            const url = `https://data.cityofnewyork.us/resource/ycrg-ses3.json?bor_=QNS`
+            processRoutes(url)
+        }
+        else if (button.textContent === "The Bronx") {
+            const url = `https://data.cityofnewyork.us/resource/ycrg-ses3.json?bor_=BX`
+            processRoutes(url)
+        }
+        else if (button.textContent === "Busway") {
+            const url = `https://data.cityofnewyork.us/resource/ycrg-ses3.json?lane_type=Busway`
+            processRoutes(url)
+        }
+        else if (button.textContent === "Single") {
+            const url = `https://data.cityofnewyork.us/resource/ycrg-ses3.json?lane_type=Single`
+            processRoutes(url)
+        }
+    });
 });
 
-laneTypes.forEach(laneType => {
-    const url = `https://data.cityofnewyork.us/resource/ycrg-ses3.json?lane_type=${laneType}`;
-    processRoutes(url);
-});
 
 
- 
     
 // Idea of the project -allows you too filter by boro and lane type too see what sbs busses operate on it
 
